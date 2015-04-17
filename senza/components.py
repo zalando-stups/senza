f171cda616747cbdcff001e5f6478cd5184ded36
Henning Jacobs <henning@jacobs1.de>
2015-04-17 14:13:06 +0200

    fix domain properties

--- senza/components.py
+++ senza/components.py
@@ -385,26 +385,37 @@ def component_load_balancer(definition, configuration, args, info):
     return definition
 
 
+def get_default_zone(region):
+    dns_conn = boto.route53.connect_to_region(region)
+    zones = dns_conn.get_zones()
+    domains = sorted([zone.name.rstrip('.') for zone in zones])
+    if not domains:
+        raise Exception('No Route53 hosted zone found')
+    return domains[0]
+
+
 def component_weighted_dns_load_balancer(definition, configuration, args, info):
     if 'Domains' not in configuration:
-        dns_conn = boto.route53.connect_to_region(args.region)
-        zones = dns_conn.get_zones()
+
         if 'MainDomain' in configuration:
-            main_domain = configuration.get('MainDomain')
+            main_domain = configuration['MainDomain']
+            main_subdomain, main_zone = main_domain.split('.', 1)
         else:
-            domains = sorted([zone.name.rstrip('.') for zone in zones])
-            if not domains:
-                raise Exception('No Route53 hosted zone found')
-            main_domain = domains[0]
+            main_zone = get_default_zone(args.region)
+            main_subdomain = info['StackName']
+
         if 'VersionDomain' in configuration:
-            version_subdomain = configuration.get('VersionDomain').split('.', 1)[0]
+            version_domain = configuration['VersionDomain']
+            version_subdomain, version_zone = version_domain.split('.', 1)
         else:
+            version_zone = get_default_zone(args.region)
             version_subdomain = '{}-{}'.format(info['StackName'], info['StackVersion'])
+
         configuration['Domains'] = {'MainDomain': {'Type': 'weighted',
-                                                   'Zone': main_domain,
-                                                   'Subdomain': info['StackName']},
+                                                   'Zone': main_zone,
+                                                   'Subdomain': main_subdomain},
                                     'VersionDomain': {'Type': 'standalone',
-                                                      'Zone': main_domain,
+                                                      'Zone': version_zone,
                                                       'Subdomain': version_subdomain}}
     return component_load_balancer(definition, configuration, args, info)
 

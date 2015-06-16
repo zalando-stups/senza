6d0f98d298716388f6c08e6655f2220e8f17b7f7
Matthias Kerk <matthias.kerk@zalando.de>
2015-06-16 15:31:36 +0200

    fix release.sh

--- release.sh
+++ release.sh
@@ -13,14 +13,14 @@ git --version
 version=$1
 
 sed -i "s/__version__ = .*/__version__ = '${version}'/" senza/__init__.py
+python3 setup.py clean
+python3 setup.py test
+python3 setup.py flake8
+
 git add senza/__init__.py
 
 git commit -m "Bumped version to $version"
 git push
-
-python3 setup.py clean
-python3 setup.py test
-python3 setup.py flake8
 
 python3 setup.py sdist upload
 python3 setup-meta.py register

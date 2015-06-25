afdecce14651cd79afc5d2d71ff42ab00aa7d666
Henning Jacobs <henning@jacobs1.de>
2015-06-25 21:37:16 +0200

    test component_iam_role

--- tests/test_components.py
+++ tests/test_components.py
@@ -1,6 +1,28 @@
 import click
 from unittest.mock import MagicMock
-from senza.components import component_elastic_load_balancer
+from senza.components import component_iam_role, component_elastic_load_balancer, get_merged_policies
+
+
+def test_component_iam_role(monkeypatch):
+    configuration = {
+        'Name': 'MyRole',
+        'MergePoliciesFromIamRoles': ['OtherRole']
+    }
+    definition = {}
+    args = MagicMock()
+    args.region = "foo"
+    monkeypatch.setattr('senza.components.get_merged_policies', MagicMock(return_value=[{'a': 'b'}]))
+    result = component_iam_role(definition, configuration, args, MagicMock(), False)
+
+    assert [{'a': 'b'}] == result['Resources']['MyRole']['Properties']['Policies']
+
+
+def test_get_merged_policies(monkeypatch):
+    iam = MagicMock()
+    iam.list_role_policies.return_value = {'list_role_policies_response': {'list_role_policies_result': {'policy_names': ['pol1']}}}
+    iam.get_role_policy.return_value = {'get_role_policy_response': {'get_role_policy_result': {'policy_document': '{"foo":"bar"}'}}}
+    monkeypatch.setattr('boto.iam.connect_to_region', lambda x: iam)
+    assert [{'PolicyDocument': {'foo': 'bar'}, 'PolicyName': 'pol1'}] == get_merged_policies(['RoleA'], 'myregion')
 
 
 def test_component_load_balancer_healthcheck(monkeypatch):

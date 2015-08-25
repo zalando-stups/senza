a299d2df565700ed864789928d59ad8bc7ce9755
Matthias Kerk <matthias.kerk@zalando.de>
2015-08-25 16:39:48 +0200

    add Choice by more then 1 domain

--- senza/cli.py
+++ senza/cli.py
@@ -306,6 +306,14 @@ class AccountArguments:
         attr = getattr(self, '__Domain', None)
         if attr is None:
             conn = boto3.client('route53')
+            domainlist = conn.list_hosted_zones()['HostedZones']
+            if len(domainlist) == 0:
+                raise AttributeError('No Domain configured')
+            elif len(domainlist) > 1:
+                domain = choice('Please select the domain',
+                                sorted(domain['Name'] for domain in domainlist))
+            else:
+                domain = domainlist[0]['Name']
             domain = conn.list_hosted_zones()['HostedZones'][0]['Name']
             setattr(self, '__Domain', domain)
             return domain

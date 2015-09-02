ad0ceba2308a3403d525f94f81b0d95532e89b6c
Henning Jacobs <henning@jacobs1.de>
2015-09-02 13:26:23 +0200

    #105 add test

--- tests/test_components.py
+++ tests/test_components.py
@@ -78,6 +78,30 @@ def test_component_load_balancer_healthcheck(monkeypatch):
         assert False, "check for supported protocols failed"
 
 
+
+def test_component_load_balancer_idletimeout(monkeypatch):
+    configuration = {
+        "Name": "test_lb",
+        "SecurityGroups": "",
+        "HTTPPort": "9999",
+        "ConnectionSettings": {"IdleTimeout": 300}
+    }
+    info = {'StackName': 'foobar', 'StackVersion': '0.1'}
+    definition = {"Resources": {}}
+
+    args = MagicMock()
+    args.region = "foo"
+
+    mock_string_result = MagicMock()
+    mock_string_result.return_value = "foo"
+    monkeypatch.setattr('senza.components.elastic_load_balancer.find_ssl_certificate_arn', mock_string_result)
+    monkeypatch.setattr('senza.components.elastic_load_balancer.resolve_security_groups', mock_string_result)
+
+    # issue 105: support additional ELB properties
+    result = component_elastic_load_balancer(definition, configuration, args, info, False)
+    assert 300 == result["Resources"]["test_lb"]["Properties"]["ConnectionSettings"]["IdleTimeout"]
+
+
 def test_component_load_balancer_namelength(monkeypatch):
     configuration = {
         "Name": "test_lb",

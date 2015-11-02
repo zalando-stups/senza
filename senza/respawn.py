018cd385d1caaeb9bb3d8a2c1fec90eb28891442
Henning Jacobs <henning@jacobs1.de>
2015-11-02 15:48:02 +0100

    #131 split methods and add docstrings

--- senza/respawn.py
+++ senza/respawn.py
@@ -9,6 +9,7 @@ RUNNING_LIFECYCLE_STATES = set(['Pending', 'InService', 'Rebooting'])
 
 
 def get_auto_scaling_group(asg, asg_name: str):
+    '''Get boto3 Auto Scaling Group by name or raise exception'''
     result = asg.describe_auto_scaling_groups(AutoScalingGroupNames=[asg_name])
     groups = result['AutoScalingGroups']
     if not groups:
@@ -16,7 +17,10 @@ def get_auto_scaling_group(asg, asg_name: str):
     return groups[0]
 
 
-def get_instances_to_terminate(group, desired_launch_config):
+def get_instances_to_terminate(group, desired_launch_config: str):
+    '''Return set of instance IDs to terminate for given Auto Scaling Group
+
+    Returns all instances where the launch configuration is not up-to-date'''
     instances_to_terminate = set()
     instances_ok = set()
     for instance in group['Instances']:
@@ -29,7 +33,8 @@ def get_instances_to_terminate(group, desired_launch_config):
     return instances_to_terminate, instances_ok
 
 
-def get_instances_in_service(group, region):
+def get_instances_in_service(group, region: str):
+    '''Get set of instance IDs with ELB "InService" state'''
     elb = boto3.client('elb')
     instances_in_service = set()
     # TODO: handle auto scaling groups without any ELB
@@ -41,6 +46,36 @@ def get_instances_in_service(group, region):
     return instances_in_service
 
 
+def scale_out(asg, asg_name, region: str, new_min_size, new_max_size, new_desired_capacity: int):
+    '''Scale out the given Auto Scaling Group to the given capacity
+    and wait for all instances to become "InService" in ELB'''
+    with Action('Scaling to {} instances..'.format(new_desired_capacity)) as act:
+        asg.update_auto_scaling_group(AutoScalingGroupName=asg_name,
+                                      MinSize=new_min_size,
+                                      MaxSize=new_max_size,
+                                      DesiredCapacity=new_desired_capacity)
+        while True:
+            current_group = get_auto_scaling_group(asg, asg_name)
+            instances_in_service = get_instances_in_service(current_group, region)
+            if len(instances_in_service) >= new_desired_capacity:
+                break
+            time.sleep(5)
+            act.progress()
+    return current_group
+
+
+def terminate_instance(asg, region: str, group, instance: str):
+    '''Terminate a single EC2 instance in given Auto Scaling Group and wait for it to become "OutOfService"'''
+
+    with Action('Terminating old instance {}..'.format(instance)) as act:
+        asg.terminate_instance_in_auto_scaling_group(InstanceId=instance, ShouldDecrementDesiredCapacity=False)
+        instances_in_service = get_instances_in_service(group, region)
+        while instance in instances_in_service:
+            time.sleep(2)
+            act.progress()
+            instances_in_service = get_instances_in_service(group, region)
+
+
 def do_respawn_auto_scaling_group(asg_name: str, group: dict, region: str,
                                   instances_to_terminate: set, instances_ok: set):
     asg = boto3.client('autoscaling', region)
@@ -49,29 +84,12 @@ def do_respawn_auto_scaling_group(asg_name: str, group: dict, region: str,
     new_min_size = group['MinSize'] + 1
     new_max_size = group['MaxSize'] + 1
     new_desired_capacity = group['DesiredCapacity'] + 1
-    asg.update_auto_scaling_group(AutoScalingGroupName=asg_name,
-                                  MinSize=new_min_size,
-                                  MaxSize=new_max_size,
-                                  DesiredCapacity=new_desired_capacity)
     # TODO: error handling (rollback in case of exception?)
     while instances_to_terminate:
-        with Action('Scaling to {} instances..'.format(new_desired_capacity)) as act:
-            while True:
-                current_group = get_auto_scaling_group(asg, asg_name)
-                instances_in_service = get_instances_in_service(current_group, region)
-                if len(instances_in_service) >= new_desired_capacity:
-                    break
-                time.sleep(5)
-                act.progress()
+        current_group = scale_out(asg, asg_name, region, new_min_size, new_max_size, new_desired_capacity)
         instance = sorted(instances_to_terminate)[0]
-        with Action('Terminating old instance {}..'.format(instance)) as act:
-            asg.terminate_instance_in_auto_scaling_group(InstanceId=instance, ShouldDecrementDesiredCapacity=False)
-            instances_in_service = get_instances_in_service(current_group, region)
-            while instance in instances_in_service:
-                time.sleep(2)
-                act.progress()
-                instances_in_service = get_instances_in_service(current_group, region)
-            instances_to_terminate.remove(instance)
+        terminate_instance(asg, region, current_group, instance)
+        instances_to_terminate.remove(instance)
 
     with Action('Resetting Auto Scaling Group to original capacity ({}-{}-{})..'.format(
                 group['MinSize'], group['DesiredCapacity'], group['MaxSize'])):
@@ -85,6 +103,7 @@ def do_respawn_auto_scaling_group(asg_name: str, group: dict, region: str,
 
 
 def respawn_auto_scaling_group(asg_name: str, region: str):
+    '''Respawn all EC2 instances in the Auto Scaling Group whose launch configuration is not up-to-date'''
     asg = boto3.client('autoscaling', region)
     group = get_auto_scaling_group(asg, asg_name)
     desired_launch_config = group['LaunchConfigurationName']

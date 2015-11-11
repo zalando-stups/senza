f44768b1485275b534cfa0bef98611f0ce598dec
Henning Jacobs <henning@jacobs1.de>
2015-11-11 14:38:08 +0100

    unify release.sh across all Python projects

--- release.sh
+++ release.sh
@@ -12,7 +12,7 @@ git --version
 
 version=$1
 
-sed -i "s/__version__ = .*/__version__ = '${version}'/" senza/__init__.py
+sed -i "s/__version__ = .*/__version__ = '${version}'/" */__init__.py
 
 # Do not tag/push on Go CD
 if [ -z "$GO_PIPELINE_LABEL" ]; then
@@ -20,14 +20,13 @@ if [ -z "$GO_PIPELINE_LABEL" ]; then
     python3 setup.py test
     python3 setup.py flake8
 
-    git add senza/__init__.py
+    git add */__init__.py
 
     git commit -m "Bumped version to $version"
     git push
 fi
 
 python3 setup.py sdist bdist_wheel upload
-python3 setup-meta.py register
 
 if [ -z "$GO_PIPELINE_LABEL" ]; then
     git tag ${version}

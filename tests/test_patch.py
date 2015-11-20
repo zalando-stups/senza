c22e111c5a55f32afc9a4692ce7b97d1c49b13e0
Henning Jacobs <henning@jacobs1.de>
2015-11-20 12:27:12 +0100

    #148 extend patch function to allow patching user data

--- tests/test_patch.py
+++ tests/test_patch.py
@@ -25,3 +25,26 @@ def test_patch_auto_scaling_group(monkeypatch):
     patch_auto_scaling_group(group, 'myregion', properties)
 
     assert new_lc['UserData'] == 'myuserdata'
+
+
+def test_patch_auto_scaling_group_taupage_config(monkeypatch):
+
+    lc = {'ImageId': 'originalimage', 'LaunchConfigurationName': 'originallc',
+          'UserData': codecs.encode(b'#firstline\nsource: oldsource', 'base64').decode('utf-8')}
+    result = {'LaunchConfigurations': [lc]}
+
+    asg = MagicMock()
+    asg.describe_launch_configurations.return_value = result
+
+    new_lc = {}
+
+    def create_lc(**kwargs):
+        new_lc.update(kwargs)
+
+    asg.create_launch_configuration = create_lc
+    monkeypatch.setattr('boto3.client', lambda x, region: asg)
+    group = {'AutoScalingGroupName': 'myasg', 'LaunchConfigurationName': 'originallc'}
+    properties = {'UserData': {'source': 'newsource'}}
+    patch_auto_scaling_group(group, 'myregion', properties)
+
+    assert new_lc['UserData'] == '#firstline\nsource: newsource\n'

c6bbe8311f586c5f480a4f917556bff19820f7e3
João Santos <joao.santos@zalando.de>
2016-06-28 10:02:22 +0200

    #252 Unit get_weights

--- tests/test_traffic.py
+++ tests/test_traffic.py
@@ -1,6 +1,6 @@
 from unittest.mock import MagicMock
 from senza.aws import SenzaStackSummary
-from senza.traffic import get_stack_versions, StackVersion
+from senza.traffic import get_stack_versions, StackVersion, get_weights
 
 
 def test_get_stack_versions(monkeypatch):
@@ -26,9 +26,11 @@ def test_get_stack_versions(monkeypatch):
     stack = MagicMock(stack_name='my-stack-1')
     resource = [
         MagicMock(resource_type='AWS::ElasticLoadBalancing::LoadBalancer'),
-        MagicMock(resource_type='AWS::Route53::RecordSet', physical_resource_id='myapp.example.org')
+        MagicMock(resource_type='AWS::Route53::RecordSet',
+                  physical_resource_id='myapp.example.org')
     ]
-    cf.Stack.return_value = MagicMock(tags=[{'Value': '1', 'Key': 'StackVersion'}], notification_arns=['some-arn'],
+    cf.Stack.return_value = MagicMock(tags=[{'Value': '1', 'Key': 'StackVersion'}],
+                                      notification_arns=['some-arn'],
                                       resource_summaries=MagicMock(all=MagicMock(return_value=resource)))
     elb.describe_load_balancers.return_value = {'LoadBalancerDescriptions': [{'DNSName': 'elb-dns-name'}]}
     monkeypatch.setattr('senza.traffic.get_stacks', MagicMock(
@@ -36,3 +38,29 @@ def test_get_stack_versions(monkeypatch):
                                                                    'StackName': 'my-stack-1'})]))
     stack_version = list(get_stack_versions('my-stack', 'my-region'))
     assert stack_version == [StackVersion('my-stack', '1', ['myapp.example.org'], ['elb-dns-name'], ['some-arn'])]
+
+
+def test_get_weights(monkeypatch):
+    get_records = MagicMock()
+    get_records.return_value = [{'Type': 'CNAME', 'Name': 'app1.example.com',
+                                 'Weight': 100, 'SetIdentifier': 'app-1'}]
+    monkeypatch.setattr('senza.traffic.get_records', get_records)
+    all_identifiers = ['app-1', 'app-2', 'app-3']
+    domains = ['app1.example.com']
+    assert get_weights(domains, 'app-1', all_identifiers) == ({'app-1': 100,
+                                                               'app-2': 0,
+                                                               'app-3': 0},
+                                                              0,
+                                                              0)
+
+    # Without weight
+    get_records.return_value = [{'Type': 'CNAME', 'Name': 'app1.example.com',
+                                 'SetIdentifier': 'app-1'}]
+
+    all_identifiers = ['app-1', 'app-2', 'app-3']
+    domains = ['app1.example.com']
+    assert get_weights(domains, 'app-1', all_identifiers) == ({'app-1': 0,
+                                                               'app-2': 0,
+                                                               'app-3': 0},
+                                                              0,
+                                                              0)

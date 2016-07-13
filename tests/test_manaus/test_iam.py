b3ca023c7792e8e2c3304d05c1239f31d84c7ee6
João Santos <joao.santos@zalando.de>
2016-07-13 09:21:34 +0200

    #219 cover more use cases on unit tests

--- tests/test_manaus/test_iam.py
+++ tests/test_manaus/test_iam.py
@@ -12,7 +12,7 @@ IAM_CERT1 = {'CertificateBody': 'body',
                  'Expiration': datetime(2022, 6, 29, 0, 0),
                  'Path': '/',
                  'ServerCertificateId': 'CERTIFICATEID',
-                 'ServerCertificateName': 'senza-example-com-20150701',
+                 'ServerCertificateName': 'senza-example-com',
                  'UploadDate': datetime(2015, 7, 1, 16, 0, 40)}}
 
 IAM_CERT1_EXP = {'CertificateBody': 'body',
@@ -43,7 +43,7 @@ def test_certificate_from_name(monkeypatch):
     monkeypatch.setattr('boto3.client', m_client)
 
     certificate1 = IAMServerCertificate.get_by_name('senza-example-com')
-    assert certificate1.name == 'senza-example-com-20150701'
+    assert certificate1.name == 'senza-example-com'
     assert certificate1.arn == 'arn:aws:iam::0000:server-certificate/senza-example-com'
 
 

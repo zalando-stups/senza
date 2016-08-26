f864cce2dd778b3259ba65631ba9efbcc62ebcac
Henning Jacobs <henning@jacobs1.de>
2016-08-26 12:26:46 +0200

    #310 first attempt to support ELB v2 (application load balancer)

---
+++ senza/components/weighted_dns_elastic_load_balancer_v2.py
@@ -0,0 +1,29 @@
+
+from senza.components.elastic_load_balancer_v2 import component_elastic_load_balancer_v2
+
+
+def component_weighted_dns_elastic_load_balancer_v2(definition, configuration, args, info, force, account_info):
+    if 'Domains' not in configuration:
+        if 'MainDomain' in configuration:
+            main_domain = configuration['MainDomain']
+            main_subdomain, main_zone = account_info.split_domain(main_domain)
+            del configuration['MainDomain']
+        else:
+            main_zone = account_info.Domain
+            main_subdomain = info['StackName']
+
+        if 'VersionDomain' in configuration:
+            version_domain = configuration['VersionDomain']
+            version_subdomain, version_zone = account_info.split_domain(version_domain)
+            del configuration['VersionDomain']
+        else:
+            version_zone = account_info.Domain
+            version_subdomain = '{}-{}'.format(info['StackName'], info['StackVersion'])
+
+        configuration['Domains'] = {'MainDomain': {'Type': 'weighted',
+                                                   'Zone': '{}.'.format(main_zone.rstrip('.')),
+                                                   'Subdomain': main_subdomain},
+                                    'VersionDomain': {'Type': 'standalone',
+                                                      'Zone': '{}.'.format(version_zone.rstrip('.')),
+                                                      'Subdomain': version_subdomain}}
+    return component_elastic_load_balancer_v2(definition, configuration, args, info, force, account_info)

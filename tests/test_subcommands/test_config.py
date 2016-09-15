92dd171ac6fe07076a3e67a16749e84c5bfb57ac
João Santos <joao.santos@zalando.de>
2016-09-15 14:10:22 +0200

    Test senza config with real files

--- tests/test_subcommands/test_config.py
+++ tests/test_subcommands/test_config.py
@@ -1,80 +1,80 @@
-from unittest.mock import MagicMock
-
-import pytest
+import yaml
+from click import get_app_dir
 from click.testing import CliRunner
 from senza.cli import cli
-from senza.exceptions import InvalidConfigKey
 
 
-@pytest.fixture()
-def mock_configuration(monkeypatch):
-    mock = MagicMock()
-    mock.dict = {"section.key": "value"}
-
-    def get(key):
-        if '.' not in key:
-            raise InvalidConfigKey("Key Error")
-        return mock.dict[key]
-
-    def setitem(key, value):
-        if '.' not in key:
-            raise InvalidConfigKey("Key Error")
-        mock.dict[key] = value
-
-    mock.__getitem__.side_effect = get
-    mock.__setitem__.side_effect = setitem
-
-    monkeypatch.setattr('senza.subcommands.config.configuration', mock)
-    return mock
+def create_config():
+    app_dir = get_app_dir("senza")
+    with open(app_dir + '/config.yaml', 'w+') as fd:
+        fd.write('{"section": {"key": "value"}}')
 
 
-def test_get_config(mock_configuration):
+def read_config():
+    app_dir = get_app_dir("senza")
+    with open(app_dir + '/config.yaml', 'r') as fd:
+        data = yaml.safe_load(fd)
+    return data
+
+
+def test_get_config():
     runner = CliRunner()
 
-    result = runner.invoke(cli,
-                           ['config', 'section.key'],
-                           catch_exceptions=False)
+    with runner.isolated_filesystem():
+        create_config()
+        result = runner.invoke(cli,
+                               ['config', 'section.key'],
+                               catch_exceptions=False)
 
     assert result.output == 'value\n'
 
 
-def test_get_config_not_found(mock_configuration):
+def test_get_config_not_found():
     runner = CliRunner()
 
-    result = runner.invoke(cli,
-                           ['config', 'section.404'],
-                           catch_exceptions=False)
+    with runner.isolated_filesystem():
+        create_config()
+        result = runner.invoke(cli,
+                               ['config', 'section.404'],
+                               catch_exceptions=False)
 
     assert result.output == ''
     assert result.exit_code == 1
 
 
-def test_get_config_no_section(mock_configuration):
+def test_get_config_no_section():
     runner = CliRunner()
 
-    result = runner.invoke(cli,
-                           ['config', '404'],
-                           catch_exceptions=False)
+    with runner.isolated_filesystem():
+        create_config()
+        result = runner.invoke(cli,
+                               ['config', '404'],
+                               catch_exceptions=False)
 
-    assert "Error: Key Error" in result.output
+    assert "Error: key does not contain a section" in result.output
 
 
-def test_set_config(mock_configuration):
+def test_set_config():
     runner = CliRunner()
 
-    result = runner.invoke(cli,
-                           ['config', 'section.new', 'value'],
-                           catch_exceptions=False)
+    with runner.isolated_filesystem():
+        create_config()
+        result = runner.invoke(cli,
+                               ['config', 'section.new', 'value'],
+                               catch_exceptions=False)
+        new_config = read_config()
 
+    assert new_config['section']['new'] == 'value'
     assert result.exit_code == 0
-    assert mock_configuration['section.new'] == 'value'
 
 
-def test_set_config_no_section(mock_configuration):
+def test_set_config_no_section():
     runner = CliRunner()
 
-    result = runner.invoke(cli,
-                           ['config', 'new', 'value'],
-                           catch_exceptions=False)
+    with runner.isolated_filesystem():
+        create_config()
+        result = runner.invoke(cli,
+                               ['config', 'new', 'value'],
+                               catch_exceptions=False)
 
-    assert "Error: Key Error" in result.output
+    assert "Error: key does not contain a section" in result.output

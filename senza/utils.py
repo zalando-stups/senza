70142be0a738e6230cc690091e3cc8ba838b5331
João Santos <joao.santos@zalando.de>
2016-10-04 08:39:45 +0200

    Re-added first step to camel_case_to_underscore

--- senza/utils.py
+++ senza/utils.py
@@ -35,7 +35,10 @@ def camel_case_to_underscore(name):
     """
     Converts name from CamelCase to snake_case
     """
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
+    # the two steps are needed to support words with sequences of more than
+    # one uppercase character
+    step1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', step1).lower()
 
 
 def pystache_render(*args, **kwargs):
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,3 +3,5 @@ from senza.utils import camel_case_to_underscore
 
 def test_camel_case_to_underscore():
     assert camel_case_to_underscore('CamelCaseToUnderscore') == 'camel_case_to_underscore'
+    assert camel_case_to_underscore('ThisIsABook') == 'this_is_a_book'
+    assert camel_case_to_underscore('InstanceID') == 'instance_id'

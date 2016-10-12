2478b797a1bc58acab70940ed9eeb85a55536ae0
João Santos <joao.santos@zalando.de>
2016-10-12 15:29:34 +0200

    Get hosted zone from route53 when creating template

--- senza/components/weighted_dns_elastic_load_balancer.py
+++ senza/components/weighted_dns_elastic_load_balancer.py
@@ -1,13 +1,26 @@
+from typing import Dict
 
+from senza.definitions import AccountArguments
 from senza.components.elastic_load_balancer import component_elastic_load_balancer
+from senza.manaus.route53 import Route53
 
 
-def component_weighted_dns_elastic_load_balancer(definition, configuration, args, info, force, account_info,
+def component_weighted_dns_elastic_load_balancer(definition,
+                                                 configuration: Dict,
+                                                 args,
+                                                 info,
+                                                 force,
+                                                 account_info: AccountArguments,
                                                  lb_component=component_elastic_load_balancer):
     if 'Domains' not in configuration:
         if 'MainDomain' in configuration:
             main_domain = configuration['MainDomain']
-            main_subdomain, main_zone = account_info.split_domain(main_domain)
+            main_subdomain, fall_back_hz = account_info.split_domain(main_domain)
+            try:
+                hosted_zone = next(Route53.get_hosted_zones(domain_name=main_domain))
+                main_zone = hosted_zone.name
+            except StopIteration:
+                main_zone = fall_back_hz
             del configuration['MainDomain']
         else:
             main_zone = account_info.Domain
@@ -15,7 +28,12 @@ def component_weighted_dns_elastic_load_balancer(definition, configuration, args
 
         if 'VersionDomain' in configuration:
             version_domain = configuration['VersionDomain']
-            version_subdomain, version_zone = account_info.split_domain(version_domain)
+            version_subdomain, fall_back_hz = account_info.split_domain(version_domain)
+            try:
+                hosted_zone = next(Route53.get_hosted_zones(domain_name=version_domain))
+                version_zone = hosted_zone.name
+            except StopIteration:
+                version_zone = fall_back_hz
             del configuration['VersionDomain']
         else:
             version_zone = account_info.Domain
@@ -27,4 +45,5 @@ def component_weighted_dns_elastic_load_balancer(definition, configuration, args
                                     'VersionDomain': {'Type': 'standalone',
                                                       'Zone': '{}.'.format(version_zone.rstrip('.')),
                                                       'Subdomain': version_subdomain}}
-    return lb_component(definition, configuration, args, info, force, account_info)
+    return lb_component(definition, configuration, args, info, force,
+                        account_info)
